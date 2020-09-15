ab791450f1478fafe700b8ad10b1c8155bd669f4
rrev <68103311+rrev4ge@users.noreply.github.com>
2020-09-15 13:23:27 +0300

    create Class project regexp

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,18 @@
 "use strict";
 
-const regexp1 = /[0-9]{7}/ig;
-const regexp2 = new RegExp("[0-9]{7}", "ig");
+const userPass = document.getElementById("password");
+const userPassRegExp = /(?=^[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g;
 
-const str = 'dsfsd 231243543 defsdf 234wsdfsfa dgferg'
-//test
-console.log(regexp1.test(str));
-console.log(regexp2.test(str));
-//search
-console.log(str.search(regexp1));
-console.log(str.search(regexp2));
-//match
-console.table(str.search(regexp1));
-console.table(str.search(regexp2));
+userPass.addEventListener("input", isCorrectInput);
 
+function isCorrectInput(event) {
+    const {
+        target: {value, style},
+    } = event;
 
-
-let userName = "Rsdfatt6jfgfgftredsvgh";
-let userPass = "sadfsdafsadfsadf";
-const userNameRegExp = /[a-zA-Z]\w*{6,20}/ig;
-const passRegExp = /[a-zA-Z]\w{8,12}/ig;
-console.log(userNameRegExp.test(userName));
-console.log(passRegExp.test(userPass));
+    if (userPassRegExp.test(value)) {
+        style.backgroundColor = "green";
+    } else if (!userPassRegExp.test(value)) {
+        style.backgroundColor = "red";
+    }
+}
